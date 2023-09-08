45e822f7b98c1fd0a3aef98ad098d00fbf6e7ca1
Alex-G-R <alex.grabski555@gmail.com>
2023-09-08 20:34:03 +0200

    Imporved quicksorting

--- quickSort.py
+++ quickSort.py
@@ -1,8 +1,10 @@
+import math
+
 def quicksort(array):
     if len(array) < 2:
         return array
     else:
-        pivot = array[0]
+        pivot = array[math.floor(len(array) / 2)]
 
         less = [i for i in array[1:] if i <= pivot]
 
